2ba70e39cea628c655449ac8f1a9d35279de8348
silviafpp <silviapacheco724@gmail.com>
2023-04-04 22:21:06 +0000

    v.1

--- volumes/php/src/index.php
+++ volumes/php/src/index.php
@@ -2,23 +2,30 @@
 <html lang="en">
 <head>
 	<title>Registration Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-		<h1>Registration Page</h1>
-		<form action="insert.php" method="post">
-<p>
-			<label for="primeiroNome">First Name:</label>
-			<input type="text" name="first_name" id="firstName">
-			</p>
-<p>
-			<label for="ultimoNome">Last Name:</label>
-			<input type="text" name="last_name" id="lastName">
-			</p>
-<p>
-			<label for="email">Email Address:</label>
-			<input type="text" name="email" id="emailAddress">
-			</p>
-			<input type="submit" value="Submit">
-		</form>
+	<div class="box">
+		<div class="container">
+			<h1>Registration Page</h1>
+			<form action="insert.php" method="post">
+				<p>
+					<label for="firstName">First Name:</label>
+					<input type="text" name="first_name" id="firstName">
+				</p>
+				<p>
+					<label for="lastName">Last Name:</label>
+					<input type="text" name="last_name" id="lastName">
+				</p>
+				<p>
+					<label for="emailAddress">Email Address:</label>
+					<input type="email" name="email" id="emailAddress">
+				</p>
+				<div class="input-field">
+					<input type="submit" value="Submit">
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
