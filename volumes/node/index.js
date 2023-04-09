f7f7483d927cfea25d4e003f82b4bd2bfd88c4b8
silviafpp <silviapacheco724@gmail.com>
2023-04-09 20:03:03 +0000

    save (not working)

--- volumes/node/index.js
+++ volumes/node/index.js
@@ -12,3 +12,16 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// const MongoClient = require('mongodb').MongoClient;
+// const url = 'mongodb://localhost:27017/mydb';
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   const collection = db.collection('keys');
+//   collection.find({}).toArray(function(err, keys) {
+//     if (err) throw err;
+//     console.log(keys);
+//     db.close();
+//   });
+// });
--- volumes/python/app.py
+++ volumes/python/app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# import redis
+
+# app = Flask(__name__)
+# r = redis.Redis(host='redis', port=6379)
+
+# @app.route('/')
+# def hello_world():
+#     r.set('comida', 'banana')
+#     r.set('xpto', 'teste')
+#     keys = [key.decode() for key in r.keys()] # convert the key values from bytes literals (redis python client default) to strings
+#     values = [value.decode() for value in r.mget(keys)]
+#     return render_template('index.html', keys=keys, values=values)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+
+from flask import Flask, request, render_template
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379)
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    key = request.form['key']
+    value = request.form['value']
+    r.set(key, value)
+    return 'Key-Value pair submitted: {}={}'.format(key, value)
 
 @app.route('/')
-def hello_world():
-    r.set('comida', 'banana')
-    r.set('xpto', 'teste')
-    keys = [key.decode() for key in r.keys()] # convert the key values from bytes literals (redis python client default) to strings
-    values = [value.decode() for value in r.mget(keys)]
-    return render_template('index.html', keys=keys, values=values)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
