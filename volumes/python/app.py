a62de59df462267c2ed55937a3fed01d73213cbe
silviafpp <silviapacheco724@gmail.com>
2023-04-12 00:17:17 +0000

    small update

--- volumes/python/app.py
+++ volumes/python/app.py
@@ -15,46 +15,5 @@ def hello_world():
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
-# from flask import Flask, request, render_template
-# import redis
-
-# app = Flask(__name__)
-# r = redis.Redis(host='localhost', port=6379, db=0)
-
-# @app.route('/submit', methods=['POST'])
-# def submit():
-#     key = request.form['key']
-#     value = request.form['value']
-#     r.set(key, value)
-#     return 'Key-Value pair submitted: {}={}'.format(key, value)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
-
-
-# from flask import Flask, render_template, request
-# import redis
-
-# app = Flask(__name__)
-# r = redis.Redis(host='localhost', port=6379)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/register', methods=['POST'])
-# def register():
-#     name = request.form['name']
-#     age = request.form['age']
-#     r.set(name, age)
-#     return 'Registration successful!'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 
 
