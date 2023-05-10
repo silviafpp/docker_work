b401c5bee83961185dad866a3fdd2b1830c368af
Sílvia Pacheco <silviapacheco724@gmail.com>
2023-05-10 09:23:40 +0000

    teste

--- volumes/node/index.js
+++ volumes/node/index.js
@@ -1,25 +1,6 @@
-// const http = require('http');
-
-// const hostname = '0.0.0.0';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 var http = require('http');
-//create a server object:
 
 http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
-
-// Console will print the message
-console.log('Server running at 3000');
+    res.write('Hello World!'); 
+    res.end(); 
+}).listen(3000); 
